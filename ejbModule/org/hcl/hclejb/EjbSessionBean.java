78268e8a475285e6d02ce277751a27df4d8e8bd2
Administrator <Administrator@SKY-20170409WXE>
2018-03-19 00:12:44 +0800

    test

--- ejbModule/org/hcl/hclejb/EjbSessionBean.java
+++ ejbModule/org/hcl/hclejb/EjbSessionBean.java
@@ -38,23 +38,24 @@ public class EjbSessionBean extends AbstractStatelessSessionBean {
 	
 	public void processInjectionBasedOnCurrentContext(Object target) {  
         Assert.notNull(target, "Target object must not be null");  
-//        WebApplicationContext cc = ContextLoader.getCurrentWebApplicationContext();  
-//        if (cc != null) {  
+        WebApplicationContext cc = ContextLoader.getCurrentWebApplicationContext();  
+        if (cc != null) {  
             AutowiredAnnotationBeanPostProcessor bpp = new AutowiredAnnotationBeanPostProcessor();  
-            ConfigurableListableBeanFactory factory = (ConfigurableListableBeanFactory) this.getBeanFactory();
-            bpp.setBeanFactory(factory);  
+//            ConfigurableListableBeanFactory factory = (ConfigurableListableBeanFactory) this.getBeanFactory();
+//            ConfigurableListableBeanFactory factory = cc.getAutowireCapableBeanFactory();
+            bpp.setBeanFactory(cc.getAutowireCapableBeanFactory());  
             bpp.processInjection(target);  
-//        }  
-//        else {  
+        }  
+        else {  
 //            if (logger.isDebugEnabled()) {  
 //                logger.debug("Current WebApplicationContext is not available for processing of " +  
 //                        ClassUtils.getShortName(target.getClass()) + ": " +  
 //                        "Make sure this class gets constructed in a Spring web application. Proceeding without injection.");  
 //            }  
-//        	System.out.println("Current WebApplicationContext is not available for processing of " +  
-//                        ClassUtils.getShortName(target.getClass()) + ": " +  
-//                        "Make sure this class gets constructed in a Spring web application. Proceeding without injection.");
-//        }  
+        	System.out.println("Current WebApplicationContext is not available for processing of " +  
+                        ClassUtils.getShortName(target.getClass()) + ": " +  
+                        "Make sure this class gets constructed in a Spring web application. Proceeding without injection.");
+        }  
     }
 
 	@Override
